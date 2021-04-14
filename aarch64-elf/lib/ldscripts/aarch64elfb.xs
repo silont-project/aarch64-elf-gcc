56c5d74b82d48fbe3a4b850e82a98e4bbfdc1cfe
Reinazhard <muh.alfarozy@gmail.com>
2021-04-14 12:00:29 +0000

    Update GCC
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- aarch64-elf/lib/ldscripts/aarch64elfb.xs
+++ aarch64-elf/lib/ldscripts/aarch64elfb.xs
@@ -7,7 +7,7 @@ OUTPUT_FORMAT("elf64-bigaarch64", "elf64-bigaarch64",
 	      "elf64-littleaarch64")
 OUTPUT_ARCH(aarch64)
 ENTRY(_start)
-SEARCH_DIR("=/home/paroji/gcc-build/../gcc-arm64/aarch64-elf/lib");
+SEARCH_DIR("=/home/reina/gcc-build/../gcc-arm64/aarch64-elf/lib");
 SECTIONS
 {
   /* Read-only sections, merged into text segment: */
--- lib/gcc/aarch64-elf/10.2.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/home/paroji/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion=Ngentod --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
