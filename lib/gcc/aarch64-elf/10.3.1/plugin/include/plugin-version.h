4a7475f17166dbd5eb7d447c1a77e4a3e000e437
Reinazhard <reinazhard@gmail.com>
2021-07-25 12:58:50 +0700

    Import ARM64 GCC 20210725
    Build completed on: Sun Jul 25 12:58:50 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/10.3.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Menrva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 10.3.1 20210725 (Menrva GCC)
    
    LLD: LLD 13.0.0 (https://github.com/llvm/llvm-project acbc0c5f0ebd8b7ebfa2eb3ae77428eb83c428c5) (compatible with GNU linkers)
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- lib/gcc/aarch64-elf/10.3.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-elf/10.3.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "10.3.1";
-static char datestamp[] = "20210721";
+static char datestamp[] = "20210725";
 static char devphase[] = "";
 static char revision[] = "";
 
