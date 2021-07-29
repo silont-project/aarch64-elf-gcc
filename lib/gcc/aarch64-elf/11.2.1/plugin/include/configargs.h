55c77c5e2bc00711de6529379e8ecfd7d00e6843
Reinazhard <reinazhard@gmail.com>
2021-07-29 11:21:56 +0700

    Import ARM64 GCC 20210729
    Build completed on: Thu Jul 29 11:21:56 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/11.2.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Menrva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.2.1 20210728 (Menrva GCC)
    
    LLD: LLD 14.0.0 (https://github.com/llvm/llvm-project c6ad3f2157ce374dbd2fd3e50699fc0303553714) (compatible with GNU linkers)
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

---
+++ lib/gcc/aarch64-elf/11.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Menrva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
