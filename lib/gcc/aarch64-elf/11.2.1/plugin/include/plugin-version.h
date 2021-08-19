351fa5e9d6457fad8008de32f5641640de9c0c8c
Reinazhard <reinazhard@gmail.com>
2021-08-19 11:36:42 +0700

    Import ARM64 GCC 20210819
    Build completed on: Thu Aug 19 11:36:42 WIB 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/11.2.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Menrva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.2.1 20210818 (Menrva GCC)
    
    LLD: LLD 14.0.0 (compatible with GNU linkers)
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- lib/gcc/aarch64-elf/11.2.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-elf/11.2.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.2.1";
-static char datestamp[] = "20210815";
+static char datestamp[] = "20210818";
 static char devphase[] = "";
 static char revision[] = "";
 
