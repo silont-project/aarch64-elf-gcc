253d66d757e78f39b62762594d475eca77a68dd6
Reinazhard <reinazhard@gmail.com>
2022-02-10 13:07:28 +0700

    Import ARM64 GCC 20220210
    Build completed on: Thu Feb 10 13:07:28 WIB 2022
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/11.2.1/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Ngentod GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.2.1 20220210 (Ngentod GCC)
    
    LLD: LLD 15.0.0 (compatible with GNU linkers)
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- lib/gcc/aarch64-elf/11.2.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-elf/11.2.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.2.1";
-static char datestamp[] = "20220205";
+static char datestamp[] = "20220210";
 static char devphase[] = "";
 static char revision[] = "";
 
